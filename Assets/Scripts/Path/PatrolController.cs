f09b45e9ad41f0c85d5aeacc3dd7a58470513c28
Antoine COLLET <ant.collet@gmail.com>
2016-10-08 18:35:08 +0200

    added patrol script; added test scene

---
+++ Assets/Scripts/Path/PatrolController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PatrolController : MonoBehaviour {
+
+	public List<Transform> waypoints = new List<Transform>();
+	public float speed	= 1f;
+
+	Vector2 target 		= new Vector3(-10f,-10f); 
+	int currentStep		= 0;
+	int nextStep		= 0;
+	Rigidbody2D body;
+
+	void Awake()
+	{
+		if (waypoints.Count > 0) {
+			transform.position = waypoints [0].position;;
+			nextStep = 1;
+		}
+	}
+
+	void Start()
+	{
+		//body = GetComponent<Rigidbody2D> ();
+	}
+	
+	void Update() {
+
+		float step = speed * Time.deltaTime;
+		transform.position = Vector2.MoveTowards (transform.position, waypoints[nextStep].position, step);
+
+		if (transform.position == waypoints[nextStep].position) {
+			nextStep = (nextStep == waypoints.Count -1) ? 0 : nextStep + 1;
+		}
+
+		transform.rotation = Quaternion.LookRotation (Vector3.forward, (Vector2)transform.position - (Vector2)waypoints[nextStep].position);
+	}
+}
---
+++ Assets/Scripts/Path/Waypoint.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Waypoint : MonoBehaviour {
+
+	public float overlapRadius = 1.1f;
+	public List<Transform> links = new List<Transform>();
+
+	void Awake()
+	{
+		Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, 1.1f);
+		int i = 0;
+		while (i < hits.Length) {
+			if (hits[i].gameObject.tag == "Waypoint" && hits[i].transform.position != transform.position)
+				links.Add(hits[i].transform);
+			i++;
+		}	
+	}
+}
