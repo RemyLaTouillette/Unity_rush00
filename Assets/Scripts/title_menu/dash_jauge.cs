1fe63b6fbbbb5da07ebb66607594b17f5ed44445
Boris MBARGA <bmbarga@e1r12p14.42.fr>
2016-10-09 21:30:17 +0200

    jauge bar added

---
+++ Assets/Scripts/title_menu/dash_jauge.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class dash_jauge : MonoBehaviour {
+
+	public float	jaugeMax = 255;
+	public float	jaugeVal = 10;
+	public Image	jaugeFg = null;
+	public Vector3	scaleTmp = Vector3.zero;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scaleTmp = this.jaugeFg.transform.localScale;
+		if (jaugeVal < jaugeMax && jaugeVal >= 0f)
+			scaleTmp.x = jaugeVal * 1f / jaugeMax;
+		else if (jaugeVal >= jaugeMax)
+			scaleTmp.x = 1;
+		else if (jaugeVal < 0)
+			scaleTmp.x = 0f;
+		this.jaugeFg.transform.localScale = scaleTmp;
+	}
+}
---
+++ Assets/Scripts/title_menu/overlay_update.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class overlay_update : MonoBehaviour {
+
+	public Text			gunType;
+	public Text			ammoCount;
+	//public player scripts.
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if (textHasChanged)
+		this.gunType.text = "Katana";
+		this.ammoCount.text = "35" + "/" + "50";
+	}
+}
